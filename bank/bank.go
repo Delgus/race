cf2b5c23905e4156c16d26b3b93264d7028eeda1
Алексей Долгов <alexey.dolgov1989@gmail.com>
2019-07-30 10:49:46 +0300

    2 версия банка

--- bank/bank.go
+++ bank/bank.go
@@ -1,11 +1,26 @@
 package bank
 
-var balance int
+var deposits = make(chan int) // Отправление вклада
+var balances = make(chan int) // Получение баланса
 
 func Deposit(amount int) {
-	balance = balance + amount
+	deposits <- amount
+}
+func Balance() int {
+	return <-balances
 }
 
-func Balance() int {
-	return balance
+func teller() {
+	var balance int // balance ограничен go-подпрограммой teller
+	for {
+		select {
+		case amount := <-deposits:
+			balance += amount
+		case balances <- balance:
+		}
+	}
+}
+
+func init() {
+	go teller() // Запуск управляющей go-подпрограммы
 }
