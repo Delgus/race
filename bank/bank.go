dcb88946d7b2cd5aa191cc09c522aa38509c0ec2
Алексей Долгов <alexey.dolgov1989@gmail.com>
2019-07-30 11:21:48 +0300

    v3 add withdraw

--- bank/bank.go
+++ bank/bank.go
@@ -1,13 +1,21 @@
 package bank
 
-var deposits = make(chan int) // Отправление вклада
-var balances = make(chan int) // Получение баланса
+var deposits = make(chan int)         // Отправление вклада
+var balances = make(chan int)         // Получение баланса
+var withdraws = make(chan int)        //Снятие со счета
+var withdrawSuccess = make(chan bool) //успешное или нет снтие со счета
 
 func Deposit(amount int) {
 	deposits <- amount
 }
+
 func Balance() int {
 	return <-balances
+}
+
+func WithDraw(amount int) bool {
+	withdraws <- amount
+	return <-withdrawSuccess
 }
 
 func teller() {
@@ -17,6 +25,13 @@ func teller() {
 		case amount := <-deposits:
 			balance += amount
 		case balances <- balance:
+		case amount := <-withdraws:
+			balance -= amount
+			if balance < 0 {
+				balance += amount
+				withdrawSuccess <- false
+			}
+			withdrawSuccess <- true
 		}
 	}
 }
--- bank/bank_test.go
+++ bank/bank_test.go
@@ -16,6 +16,22 @@ func TestRaceDeposit(t *testing.T) {
 	}
 	group.Wait()
 	if Balance() != 1000000 {
-		t.Errorf("unexpected balance: value - %d expect 1000000", Balance() )
+		t.Errorf("unexpected balance: value - %d expect 1000000", Balance())
+	}
+}
+
+func TestRaceWithDraw(t *testing.T) {
+	Deposit(1000000)
+	var group sync.WaitGroup
+	group.Add(10000)
+	for i := 0; i < 10000; i++ {
+		go func() {
+			WithDraw(100)
+			group.Done()
+		}()
+	}
+	group.Wait()
+	if Balance() != 1000000 {
+		t.Errorf("unexpected balance: value - %d expect 1000000", Balance())
 	}
 }
