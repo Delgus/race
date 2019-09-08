ae4216c7458ba2a1aede6aa537dbcfa7c1bb0df9
delgus <alexdolgovak47@mail.ru>
2019-09-08 23:50:43 +0300

    Доработка первой версии банка

--- bank/v1/bank_test.go
+++ bank/v1/bank_test.go
@@ -8,6 +8,12 @@ func TestDeposit(t *testing.T) {
 	Deposit(100)
 	Balance()
 	if Balance() != 100 {
-		t.Errorf("unexpected balance: value - %d expect 100", Balance() )
+		t.Errorf("unexpected balance: value - %d expect 100", Balance())
+	}
+}
+
+func BenchmarkBalance(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Deposit(1000)
 	}
 }
