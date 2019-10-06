77986f8da242155225cfcc862a28ee0161fd7a00
delgus <alexdolgovak47@mail.ru>
2019-10-06 21:36:44 +0300

    Правка кода

--- bank/v6/bank.go
+++ bank/v6/bank.go
@@ -14,10 +14,6 @@ func Deposit(amount int) {
 	mu.Unlock()
 }
 
-func deposit(amount int) {
-	balance = balance + amount
-}
-
 //Balance ...
 func Balance() int {
 	mu.Lock()
@@ -26,17 +22,24 @@ func Balance() int {
 	return b
 }
 
+//WithDraw ...
 func WithDraw(amount int) bool {
 	mu.Lock()
-	defer func() {
-		mu.Unlock()
-	}()
-	deposit(-amount)
+	defer mu.Unlock()
+	withDraw(amount)
 	if balance < 0 {
 		deposit(amount)
 		return false
 	}
 	return true
+}
+
+func deposit(amount int) {
+	balance = balance + amount
+}
+
+func withDraw(amount int) {
+	balance = balance - amount
 }
 
 func setBalance(count int) {
